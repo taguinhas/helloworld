69afa0cbeda779620772ee0dec715e0a0dbc84f4
taguinhas <tiago.boaventura.nunes@gmail.com>
2024-11-11 21:39:25 +0000

    Added option for mul _ Step12

--- src/Application.java
+++ src/Application.java
@@ -4,9 +4,15 @@ public class Application {
     public static void main(String[] args){
         Scanner scanner  = new Scanner(System.in);
 
+        String operation = scanner.nextLine();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a + b);
+        if(operation.equals("sum")){
+            System.out.println(a + b);
+        }
+        else if(operation.equals("mul")){
+            System.out.print(a * b);
+        }
     }
 }
